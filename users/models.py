e181c20460196b4993a2ca50881a671004bd800e
Sanjeev Reddy <sanjeev96red@gmail.com>
2019-04-16 15:29:15 -0500

    User sets their location on a map

--- users/models.py
+++ users/models.py
@@ -2,11 +2,11 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.gis.db import models as geomodels
 
 # Create your models here.
 class Profile(AbstractUser):
-    location_city = models.CharField(max_length=50, blank=True, null=True)
-    location_state = models.CharField(max_length=50, blank=True, null=True)
+    location = geomodels.PointField()
 
     def __str__(self):
         return self.username
